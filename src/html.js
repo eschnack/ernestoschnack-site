ea4d3696ec47251b6756a1a5a5e9aee6e0cf285a
Ernesto Schnack <eschnack@tvnmedia.com>
2020-03-26 22:35:48 -0500

    google verif

--- src/html.js
+++ src/html.js
@@ -11,6 +11,10 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <meta
+          name="google-site-verification"
+          content="JsgMbEr06fIWgCpH3medokdeVUxO2cFEBwPDbxUVN9U"
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
