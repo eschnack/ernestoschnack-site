996a22dd079571ca269d0361a7f4dfa31a2cb367
eschnack <eschnack@gmail.com>
2023-03-14 15:35:20 -0500

    gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,15 +95,22 @@ module.exports = {
         ],
       },
     },
+
     {
-      resolve: `gatsby-plugin-gtag`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // your google analytics tracking id
-        trackingId: `UA-9191843-1`,
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // enable ip anonymization
-        anonymize: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-9191843-1", // Google Analytics / GA
+          "G-79QN07R377",
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
       },
     },
     {
